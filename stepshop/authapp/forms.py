d45d28e353588282d4407447ffe85a9a92714922
Курманбек Султан <@itstep.loc>
2023-03-28 20:50:57 +0600

    lesson_08 files

--- stepshop/authapp/forms.py
+++ stepshop/authapp/forms.py
@@ -1,55 +1,63 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from authapp.models import ShopUser
 
 
 class ShopUserLoginForm(AuthenticationForm):
     class Meta:
-        model = ShopUser,
+        model = ShopUser
         fields = ('username', 'password')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['class'] = 'form-control py-4'
             field.help_text = ''
-
 
 
 class ShopUserRegisterForm(UserCreationForm):
     class Meta:
-        model = ShopUser,
-        fields = ('username',
-                  'first_name',
-                  'password1',
-                  'password2',
-                  'email',
-                  'age',
-                  'avatar')
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2', 'email', 'avatar', 'age')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['class'] = 'form-control py-4'
             field.help_text = ''
+        self.fields['avatar'].help_text = 'Загружайте изображения в формате PNG, JPG или JPEG'
+        self.fields['age'].help_text = 'Укажите свой возраст'
 
     def clean_age(self):
         data = self.cleaned_data['age']
-
         if data < 18:
-            raise forms.ValidationError("Вы слишком молоды!")
+            raise forms.ValidationError('Вы слишком молоды')
+        return data
 
+    def clean_avatar(self):
+        data = self.cleaned_data['avatar']
+        if data.size > 3145728:
+            raise forms.ValidationError('Размер файла не должен превышать 3 Мб')
         return data
 
 
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'password', 'email', 'avatar', 'age')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+            field.help_text = ''
+        self.fields['avatar'].help_text = 'Загружайте изображения в формате PNG, JPG или JPEG'
+        self.fields['age'].help_text = 'Укажите свой возраст'
+        self.fields['password'].widget = forms.HiddenInput()
 
-
-
-
-
-
-
-
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError('Вы слишком молоды')
+        return data
