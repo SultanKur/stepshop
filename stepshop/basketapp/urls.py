59d576876c6eefbe0bdb144742d7b261b0ea95ff
Курманбек Султан <@itstep.loc>
2023-04-11 20:19:19 +0600

    lesson_12 files

--- stepshop/basketapp/urls.py
+++ stepshop/basketapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from basketapp.views import basket_remove, basket_add, basket
+from basketapp.views import basket_remove, basket_add, basket, basket_edit
 
 app_name = 'basketapp'
 
 urlpatterns = [
     path('', basket, name='view'),
     path('add/<int:pk>/', basket_add, name='add'),
+    path('edit/<int:pk>/<int:quantity>', basket_edit, name='edit'),
     path('remove/<int:pk>/', basket_remove, name='remove'),
 ]
