b6bf8d3c675be3c3a842babe528aa0f2d3693f3d
Курманбек Султан <@itstep.loc>
2023-04-18 21:29:46 +0600

    lesson_14 files

--- stepshop/authapp/models.py
+++ stepshop/authapp/models.py
@@ -5,5 +5,7 @@ from django.db import models
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
 
-    age = models.PositiveIntegerField(verbose_name='возраст')
+    age = models.PositiveIntegerField(verbose_name='возраст',)
+
+
 
